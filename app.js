ae4796ce7e96246e3ea0ebaef61f2795cb8d2667
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-11-26 21:18:48 +0200

    first

--- app.js
+++
@@ -1,7 +0,0 @@
-import config from './config/config.json';
-import {User, Product} from './models';
-
-console.log(`Application name: ${config.name}`);
-
-const user = new User();
-const product = new Product();
--- models/index.js
+++
@@ -1,7 +0,0 @@
-import User from './​User';
-import Product from './Product';
-
-export {
-  User,
-  Product
-};

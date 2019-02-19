abf81f2fcb0727c573f4ede730eec4ad004464da
Andriy Ivashchenko <andr.ivas12@gmail.com>
2019-02-19 21:22:12 +0200

    hw6: postgres and sequelize

--- express/routes/api/products.js
+++ express/routes/api/products.js
@@ -1,39 +1,49 @@
 import express from 'express';
-import path from 'path';
-import lowdb from 'lowdb';
-import generate from 'nanoid/non-secure/generate';
+import pdb from '../../models';
 
 const router = express.Router();
 
-// local database
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync(path.join(__dirname, '../../db.json'));
-const db = lowdb(adapter);
-
 router.get('/', (req, res) => {
-  res.send(
-    db.get('products')
-      .value()
-  );
+  return pdb.Product.findAll()
+    .then(products => res.send(products))
+    .catch((err) => {
+      console.log('There was an error querying products', JSON.stringify(err));
+      return res.send(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   const productId = req.params.id;
-  const singleProduct = db.get('products').find({ id: +productId }).value();
-  if (!singleProduct) {
-    res.status(404).json({ status: 404, message: 'Product Not found.' });
-  } else {
-    res.send(singleProduct);
-  }
+  return pdb.Product.findById(productId)
+    .then(product => {
+      if (!product) {
+        return res.status(404).json({ status: 404, message: 'Product Not found.' });
+      }
+      res.send(product);
+    })
+    .catch((err) => {
+      console.log('There was an error querying product', JSON.stringify(err));
+      return res.send(err);
+    });
 });
 
 router.get('/:id/reviews', (req, res) => {
   const productId = req.params.id;
-  const reviews = db.get('reviews').find({ product_id: +productId }).value();
-  if (!reviews) {
-    res.status(404).json({ status: 404, message: 'Reviews for this product Not found.' });
-  }
-  res.send(reviews);
+  return pdb.Review.findAll({
+    where: {
+      product_id: productId
+    }
+  })
+    .then(reviews => {
+      if (!reviews) {
+        return res.status(404).json({ status: 404, message: 'Reviews for this product Not found.' });
+      }
+      res.send(reviews);
+    })
+    .catch((err) => {
+      console.log('There was an error querying reviews', JSON.stringify(err));
+      return res.send(err);
+    });
 });
 
 router.post('/', (req, res) => {
@@ -42,14 +52,17 @@ router.post('/', (req, res) => {
     res.status(404).json({ status: 400, message: 'Missing required parameter - name' });
   } else {
     const newProduct = {
-      id: generate('1234567890', 5),
       ...req.body
     };
-    db.get('products')
-      .push(newProduct)
-      .write();
-    res.header('Location', `http://localhost:3000/api/v1.0/products/${newProduct.id}`);
-    res.json(newProduct);
+    return pdb.Product.create(newProduct)
+      .then(product => {
+        res.header('Location', `http://localhost:3000/api/v1.0/products/${product.id}`);
+        res.send(product);
+      })
+      .catch((err) => {
+        console.log('***There was an error creating a Product', JSON.stringify(product))
+        return res.status(400).send(err);
+      });
   }
 });
 
